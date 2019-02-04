5eb129197b91c2e7509655e9743bbe6cb1512c5b
Yakov Fat <YakovFat@yandex.ru>
2019-02-04 20:21:23 +0300

    gt_2

--- 1.5.Pep8/config.py
+++ 1.5.Pep8/config.py
@@ -1,6 +1,2 @@
-GMAIL_SMTP = "smtp.gmail.com"
-GMAIL_IMAP = "imap.gmail.com"
-
 user_email = 'login@gmail.com'
 password_email = 'qwerty'
-
--- 1.5.Pep8/main.py
+++ 1.5.Pep8/main.py
@@ -3,13 +3,17 @@ import smtplib
 import imaplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from config import user_email, password_email, GMAIL_IMAP, GMAIL_SMTP
+from config import user_email, password_email
 
 
-class User:
-    def __init__(self, user_login, password):
+class MailHandler:
+    def __init__(self, user_login, password, DEFAULT_SMTP = "smtp.gmail.com",
+                 DEFAULT_IMAP = "imap.gmail.com", MAIL_SELECT = "inbox"):
         self.user_login = user_login
         self.password = password
+        self.DEFAULT_SMTP = DEFAULT_SMTP
+        self.DEFAULT_IMAP = DEFAULT_IMAP
+        self.MAIL_SELECT = MAIL_SELECT
 
     def send_a_message(self, recipients, subject, message):
         msg = MIMEMultipart()
@@ -18,7 +22,7 @@ class User:
         msg['Subject'] = subject
         msg.attach(MIMEText(message))
 
-        ms = smtplib.SMTP(GMAIL_SMTP, 587)
+        ms = smtplib.SMTP(self.DEFAULT_SMTP, 587)
         # identify ourselves to smtp gmail client
         ms.ehlo()
         # secure our email with tls encryption
@@ -33,23 +37,24 @@ class User:
         # send end
 
     def receive_a_message(self, header):
-        mail = imaplib.IMAP4_SSL(GMAIL_IMAP)
+        mail = imaplib.IMAP4_SSL(self.DEFAULT_IMAP)
         mail.login(self.user_login, self.password)
         mail.list()
-        mail.select("inbox")
+        mail.select(self.MAIL_SELECT)
         criterion = '(HEADER Subject "%s")' % header if header else 'ALL'
         result, data = mail.uid('search', None, criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
         result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
         raw_email = data[0][1]
-        email_message = email.message_from_string(str(raw_email))
+        email_message = email.message_from_bytes(raw_email)
         mail.logout()
         # end recieve
         return email_message
 
 
 if __name__ == '__main__':
-    user_1 = User(user_email, password_email)
-    user_1.send_a_message(['vasya@email.com', 'petya@email.com'], 'Subject', 'Message')
+    user_1 = MailHandler(user_email, password_email)
+    user_1.send_a_message(['vasya@email.com', 'petya@email.com'],
+                          'Subject', 'Message')
     user_1.receive_a_message(None)
