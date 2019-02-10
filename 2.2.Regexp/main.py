177ee0b38e1a326b7fee68934d396f622d6f2379
Yakov Fat <YakovFat@yandex.ru>
2019-02-10 21:32:48 +0300

    gt

--- 2.2.Regexp/main.py
+++ 2.2.Regexp/main.py
@@ -1,39 +1,54 @@
-
 import re
 import csv
-with open("phonebook_raw.csv", encoding='utf-8') as f:
+from pprint import pprint
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 pop = contacts_list.pop([0][0])
-# TODO 1: выполните пункты 1-3 ДЗ
+people_dict = {}
 for contact in contacts_list:
-    new_contact = ','.join(contact)
-    telephone = "(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(-*)(\d{3})(-*)(\d{2})(-*)" \
+    fio = re.split("\s+", contact[0] + " " + contact[1] + " " + contact[2])
+    if "" in fio:
+        fio.remove("")
+    fio = tuple(fio)
+    empty_dict = {"organization": "", "position": "", "phone": "", "email": ""}
+    people_dict.setdefault(fio, empty_dict)
+    if len(people_dict[fio]["organization"]) == 0:
+        people_dict[fio]["organization"] = contact[3]
+    people_dict[fio]["position"] = (
+        lambda var: var if len(var) > 0 else people_dict[fio]["position"])(
+        contact[4])
+    pattern = "(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(-*)(\d{3})(-*)(\d{2})(-*)" \
               "(\d+)(\s*)(\(*)([а-яёА-ЯЁ]{3}.)*(\s*)(\d{4})*(\)*)"
-    result_1 = re.sub(telephone, r"+7(\4)\8-\10-\12\13\15\17", new_contact)
-    pattern_2 = "^([а-яёА-ЯЁ]+)(\s|\W)([а-яёА-ЯЁ]+)(\s|\W)([а-яёА-ЯЁ]+)*(,+)([а-яёА-ЯЁ]+)*(,+)"
-    result_2 = re.sub(pattern_2, r"\1,\3,\5,\7,", result_1)
-    print(result_2)
+    contact[5] = re.sub(pattern, r"+7(\4)\8-\10-\12\13\15\17", contact[5])
+    people_dict[fio]["phone"] = (
+        lambda var: var if len(var) > 0 else people_dict[fio]["phone"])(
+        contact[5])
+    people_dict[fio]["email"] = (
+        lambda var: var if len(var) > 0 else people_dict[fio]["email"])(
+        contact[6])
 
+for k in people_dict.keys():
+    if len(k) < 3:
+        for k2 in people_dict.keys():
+            if k[0] == k2[0] and len(k2) == 3:
+                for i in people_dict[k]:
+                    if people_dict[k][i] != '':
+                        people_dict[k2][i] = people_dict[k][i]
+                new_people_dict = people_dict.copy()
+                del new_people_dict[k]
 
+pprint(new_people_dict)
+final_people_csv = []
+final_people_csv.append(pop)
+for i in new_people_dict:
+    final_people_csv.append([*i, new_people_dict[i]['organization'],
+                             new_people_dict[i]['position'],
+                             new_people_dict[i]['phone'],
+                             new_people_dict[i]['email']])
 
-
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-# with open("phonebook.csv", "w") as f:
-#     datawriter = csv.writer(f, delimiter=',')
-#     # Вместо contacts_list подставьте свой список
-#     datawriter.writerows(contacts_list)
-
-# lastname,firstname,surname
-# ^([а-яёА-ЯЁ]+)(\s|\W)([а-яёА-ЯЁ]+)(\s|\W)([а-яёА-ЯЁ]+)*(\W)
-# $1 $3 $5,
-
-
-
-# telephone (\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(-*)(\d{3})(-*)(\d{2})(-*)(\d+)(\s*)(\(*)([а-яёА-ЯЁ]{3}.)*(\s*)(\d{4})*(\)*)
-# +7(\4)\8-\10-\12\13\15\17
-
-# email
-# [a-zA-Z.]+.[a-zA-Z]+|\d+@
+print(final_people_csv)
+with open("phonebook.csv", "w", encoding="utf-8", newline='') as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(final_people_csv)
