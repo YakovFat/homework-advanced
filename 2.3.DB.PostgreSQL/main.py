d02ae239e3d7e0df2fe19c07969851bc7dfc9e19
Yakov Fat <YakovFat@yandex.ru>
2019-02-24 21:07:20 +0300

    gt

--- 2.3.DB.PostgreSQL/main.py
+++ 2.3.DB.PostgreSQL/main.py
@@ -1,33 +1,36 @@
 import psycopg2 as pg
 
-dbname = ''
-user = ''
+dbname = 'mydb'
+user = 'rk0f'
 
 
-def create():
+def create_db():
     with pg.connect(dbname=dbname, user=user) as conn:
         with conn.cursor() as cur:
-            cur.execute("""
-                create table Student (
-                    id serial PRIMARY KEY,
-                    name varchar(100) not null,
-                    gpa numeric(10,2),
-                    birth timestamp with time zone
-                    );
-                """)
-            cur.execute("""
-                create table Course (
-                    id serial PRIMARY KEY,
-                    name varchar(100) not null
-                    );
-                """)
-            cur.execute("""
-                create table Student_Course (
-                    id serial PRIMARY KEY,
-                    student_id INTEGER REFERENCES Student(id),
-                    course_id INTEGER REFERENCES Course(id)
-                    );
-                """)
+            try:
+                cur.execute("""
+                    create table Student (
+                        id serial PRIMARY KEY,
+                        name varchar(100) not null,
+                        gpa numeric(10,2),
+                        birth timestamp with time zone
+                        );
+                    """)
+                cur.execute("""
+                    create table Course (
+                        id serial PRIMARY KEY,
+                        name varchar(100) not null
+                        );
+                    """)
+                cur.execute("""
+                    create table Student_Course (
+                        id serial PRIMARY KEY,
+                        student_id INTEGER REFERENCES Student(id) ON DELETE CASCADE,
+                        course_id INTEGER REFERENCES Course(id) ON DELETE CASCADE
+                        );
+                    """)
+            except pg.ProgrammingError:
+                print("Already exists")
 
 
 def get_students(course_id):
@@ -44,19 +47,19 @@ def add_student(student):
     with pg.connect(dbname=dbname, user=user) as conn:
         with conn.cursor() as cur:
             cur.execute("""
-               insert into student (name, gpa, birth) values (%s, %s, %s);
+               insert into student (name, gpa, birth) values (%s, %s, %s) RETURNING id;
                """, (student['name'], student['gpa'], student['birth']))
+            return cur.fetchone()[0]
 
 
 def add_students(course_id, students):
     for i in students:
-        add_student(i)
+        id_s = add_student(i)
         with pg.connect(dbname=dbname, user=user) as conn:
             with conn.cursor() as cur:
                 cur.execute("""
                     select id from student where name = (%s);
                     """, (i['name'],))
-                id_s = cur.fetchall()[0][0]
                 cur.execute("""
                    insert into Student_Course (student_id, course_id) values
                    (%s, %s);
@@ -69,8 +72,9 @@ def get_student(student_id):
             cur.execute("""
                 select * from student where id = (%s);
                 """, (student_id,))
-            return cur.fetchall()
+            return cur.fetchone()
 
 
 if __name__ == '__main__':
-    print(get_students(1))
+    # create_db()
+    pass
