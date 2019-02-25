4c992efdf469d6266d16cc8dc09414f7f8f77a9b
Yakov Fat <YakovFat@yandex.ru>
2019-02-25 21:08:12 +0300

    gt

--- 2.4.DB.Mongo.ORM/main.py
+++ 2.4.DB.Mongo.ORM/main.py
@@ -11,13 +11,15 @@ def read_data(csv_file, db):
     """
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
+        list_row = []
         reader = csv.DictReader(csvfile)
         for row in reader:
             row = dict(row)
             row['Цена'] = int(row['Цена'])
             row['Дата'] += '.2019'
             row['Дата'] = datetime.strptime(row['Дата'], '%d.%m.%Y')
-            db.insert_one(row)
+            list_row.append(row)
+        db.insert_many(list_row)
 
 
 def find_cheapest(db):
@@ -33,25 +35,26 @@ def find_by_name(name, db):
     Найти билеты по имени исполнителя (в том числе – по подстроке),
     и выведите их по возрастанию цены
     """
+    name = re.escape(name)
     regex = re.compile(name)
     return list(db.find({'Исполнитель': regex}).sort("Цена", 1))
 
 
 def find_by_date(date_1, date_2, db):
-    """
-    Найти билеты по имени исполнителя (в том числе – по подстроке),
-    и выведите их по возрастанию цены
-    """
-    date_1 = datetime(*date_1)
-    date_2 = datetime(*date_2)
+    date_1 = datetime_convert(date_1)
+    date_2 = datetime_convert(date_2)
     return list(db.find({'Дата': {'$gt': date_1, '$lt': date_2}}))
+
+
+def datetime_convert(date):
+    return datetime.strptime(date, '%d.%m.%Y')
 
 
 if __name__ == '__main__':
     client = MongoClient()
     mydb = client.homework
-    ticket = mydb.ticket
-    # read_data('artists.csv', ticket)
-    # print(find_cheapest(ticket))
-    # print(find_by_name('в', ticket))
-    print(find_by_date((2019, 4, 22, 0, 0), (2019, 11, 22, 0, 0), ticket))
+    mydb_ticket = mydb.ticket
+    # read_data('artists.csv', mydb_ticket)
+    # print(find_cheapest(mydb_ticket))
+    # print(find_by_name('в', mydb_ticket))
+    print(find_by_date("22.4.2019", "22.11.2019", mydb_ticket))
